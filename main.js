b46eb06c394c282fab23bde4a1f272f114cf69cd
Lawrence <lawrence.perez@cyberservices.com>
2021-10-03 01:21:00 +1000

    search function

--- main.js
+++ main.js
@@ -1,28 +1,47 @@
-const API_Data = {
-    url: "https://pokeapi.co/api/v2/",
-    type: 'pokemon',
-    id: '26',
-};
+const pokedex_url = "https://pokeapi.co/api/v2/";
+const creature_type = "pokemon";
 
-const {url, type, id} = API_Data;
+api_data_search();
 
-const API_URL = `${url}${type}/${id}`;
+function api_data_search() {
+    var pokemon_id = document.querySelector('#idsearch').value.toLowerCase();
+    var api_data = { 
+        url: pokedex_url,
+        type: creature_type,
+        id: pokemon_id
+    }
 
-fetch(API_URL)
-    .then( (data) => data.json() )
-    .then ( (pokemon) => generateHTML(pokemon) )
-    
+    var {url, type, id} = api_data;
+    var api_url = `${url}${type}/${id}`;
+
+    fetch_data(api_url);
+}
+
+function fetch_data(api_url) {
+    fetch(api_url)
+        .then( (data) => data.json() )
+        .then( (pokemon) => generateHTML(pokemon) )  
+        .catch( (error) => generateError() ) 
+}
+
 const generateHTML = (data) => {
     console.log(data);
-    const html = `
-        <div class="name">${data.name}</div>
+    var html = `
+        <div class="name">${data.id}</div>
         <img src=${data.sprites.front_default} />
         <div class="details">
-            <span>Height: ${data.height} units</span>
-            <span>Weight: ${data.weight} units</span>
+            <p>Name: ${data.name}</p>
+            <p>Height: ${data.height} units</p>
+            <p>Weight: ${data.weight} units</p>
         </div>
     `
 
+    const pokemonDiv = document.querySelector('.pokemon')
+    pokemonDiv.innerHTML = html;
+}
+
+const generateError = () => {
+    var html = `<p> No Pokemon Found! </p>`
     const pokemonDiv = document.querySelector('.pokemon')
     pokemonDiv.innerHTML = html;
 }
