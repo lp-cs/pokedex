4bbe79500272665de35c758461fa94261e454ae6
Lawrence <lawrence.perez@cyberservices.com>
2021-10-03 17:29:58 +1100

    sprite buttons added

--- main.js
+++ main.js
@@ -20,28 +20,90 @@ function api_data_search() {
 function fetch_data(api_url) {
     fetch(api_url)
         .then( (data) => data.json() )
-        .then( (pokemon) => generateHTML(pokemon) )  
+        .then( (pokemon) => generateHTML(pokemon) )
         .catch( (error) => generateError() ) 
 }
 
 const generateHTML = (data) => {
     console.log(data);
-    var html = `
-        <div class="name">${data.id}</div>
-        <img src=${data.sprites.front_default} />
-        <div class="details">
-            <p>Name: ${data.name}</p>
-            <p>Height: ${data.height} units</p>
-            <p>Weight: ${data.weight} units</p>
-        </div>
-    `
 
-    const pokemonDiv = document.querySelector('.pokemon')
-    pokemonDiv.innerHTML = html;
+    const sprites = data.sprites;
+    const moves = data.moves;
+    const abilities = data.abilities;
+
+    const pokemon_name_div = document.querySelector('.pokemon_name');
+    const pokemon_sprite_div = document.querySelector('.pokemon_sprites');
+    const pokemon_details_div = document.querySelector('.pokemon_details');
+    const pokemon_ability_div = document.querySelector('.ability_list');
+    const pokemon_moves_div = document.querySelector('.move_list');
+
+    var pokemon_details = `
+        <div class="details">
+            <p>ID Number: ${data.id}</p>
+            <p>Height: ${data.height} dm</p>
+            <p>Weight: ${data.weight} hg</p>
+        </div>
+    `;
+
+    abilities.forEach(abilities =>  pokemon_ability_div.innerHTML += 
+        "<li>" + abilities.ability.name + "</li>"
+    );
+
+    pokemon_sprite_div.innerHTML = `
+        <img id="front_default_sprite" class="pokemon_sprite" src="${sprites.front_default}" />
+        <img id="back_default_sprite" class="pokemon_sprite" src="${sprites.back_default}" />
+        <img id="front_shiny_sprite" class="pokemon_sprite" src="${sprites.front_shiny}" />
+        <img id="back_shiny_sprite" class="pokemon_sprite" src="${sprites.back_shiny}" />
+    `;
+    
+    display_sprite("front_default");
+
+    pokemon_name_div.innerHTML = 
+        "<h1>" + data.name.toUpperCase() + "</h1>"
+    ;
+
+    pokemon_details_div.innerHTML = pokemon_details;
+
+    moves.forEach(moves =>  pokemon_moves_div.innerHTML += 
+        "<li>" + moves.move.name + "</li>"
+    );
 }
 
 const generateError = () => {
     var html = `<p> No Pokemon Found! </p>`
-    const pokemonDiv = document.querySelector('.pokemon')
-    pokemonDiv.innerHTML = html;
+    const error_message = document.querySelector('.pokemon_details');
+    error_message.innerHTML = html;
+}
+
+function display_sprite(sprite_details){
+    const front_sprite =  document.querySelector('#front_default_sprite');
+    const back_sprite =  document.querySelector('#back_default_sprite');
+    const front_shiny = document.querySelector('#front_shiny_sprite');
+    const back_shiny = document.querySelector('#back_shiny_sprite');
+
+    hide_sprite();
+
+    switch(sprite_details) {
+        case "front_default":
+            front_sprite.style.display = "block"; 
+          break;
+        case "back_default":  
+            back_sprite.style.display = "block"; 
+          break;
+        case "front_shiny":
+            front_shiny.style.display = "block"; 
+          break;
+        case "back_shiny":
+            back_shiny.style.display = "block"; 
+          break;
+        default:
+            return false;
+      }
+}
+
+function hide_sprite(){
+    const pokemon_sprites = document.querySelectorAll('.pokemon_sprite');
+    pokemon_sprites.forEach(function(sprite) {
+       sprite.style.display = "none";
+     });
 }
